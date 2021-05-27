c7ff7d3e313d8d65edb0e1dcc3015d867f90d683
pe2ya <32875661+pe2ya@users.noreply.github.com>
2021-05-27 10:27:10 +0200

    Add files via upload
    I changed almost all code for ignore "/n" sing

--- pocet mezer/src/mail.java
+++ pocet mezer/src/mail.java
@@ -1,26 +1,28 @@
 import java.io.*;
+import java.util.Scanner;
 
 
 public class mail {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		
 		File file = new File("M:\\pocet mezer\\lm.txt");
-		FileInputStream fn = new FileInputStream(file);
-		InputStreamReader in = new InputStreamReader(fn);
-		BufferedReader br = new BufferedReader(in);
-		String text;
+
+		
+		try(Scanner sc = new Scanner(new FileInputStream(file))){
 		
 		int WordCount = 0;
 		int SpaceCount = 0;
-		while((text = br.readLine()) != null) {
-			String[] wordList = text.split("\\s+");
-			WordCount +=wordList.length;
-			SpaceCount = WordCount -1;
+		while(sc.hasNext()) {
+			sc.next();
+			WordCount++;
 		}	
-		
+		 SpaceCount = WordCount - 1;
 		System.out.println("Words: " + WordCount);
 		System.out.println("Spaces: " + SpaceCount);
+	}catch (FileNotFoundException e) {
+		System.out.println("Something went wrong");
 	}
-
+	}
+	
 }
